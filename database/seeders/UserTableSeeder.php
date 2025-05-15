2cfc1b24f0c7a37f8a37729ae9b35280f49355ed
Sumon <eubsumon@gmail.com>
2025-05-15 13:22:24 +0600

    Fix seeder Error Import

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\role;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserTableSeeder extends Seeder
